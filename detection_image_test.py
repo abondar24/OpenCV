86a9708082a2922c78dd5e1674807b6c05676030
alexabon <alexabon@cisco.com>
2016-09-05 23:53:27 -0700

    detection image cleanup

--- detection_image_test.py
+++ detection_image_test.py
@@ -8,11 +8,9 @@ flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 
 
 image = cv2.imread('hard.jpg')
-faces = face_cascade.detectMultiScale(image, scaleFactor = scale_factor, minNeighbors = min_neighbors,
-                                      minSize = min_size, flags = flags)
+faces = face_cascade.detectMultiScale(image, scaleFactor=scale_factor, minNeighbors=min_neighbors,
+                                      minSize=min_size, flags=flags)
 
-
-
-for( x, y, w, h ) in faces:
+for(x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
     cv2.imwrite('out.jpg', image)
