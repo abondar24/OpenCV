348ebc9c02569cf2199f6be80838f2ee5c2b4706
abondar24 <abondar1992@gmail.com>
2017-03-06 20:03:35 -0800

    pixel manipulations

--- CPP/mainwindow.h
+++ CPP/mainwindow.h
@@ -18,6 +18,15 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void salt(cv::Mat &image, int n);
+    void colorReduce(cv::Mat &image, int div =64);
+    void colorReduce(cv::Mat &image,cv::Mat &result, int div=64);
+    void colorReduceIter(cv::Mat &image, int div =64);
+    void sharpen(cv::Mat &image,cv::Mat &result);
+    void sharpen2D(cv::Mat &image,cv::Mat &result);
+    void roiDemo(cv::Mat &image);
+
+
     ~MainWindow();
 
 private slots:
@@ -25,9 +34,23 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
 private:
     Ui::MainWindow *ui;
     cv::Mat image;
+    void displayImage(cv::Mat &image);
+
 };
 
 #endif // MAINWINDOW_H
